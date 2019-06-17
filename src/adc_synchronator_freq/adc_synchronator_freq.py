f365c909b6d3ee686fd1c11882273fcd3c10722f
Franco Curotto <francocurotto@gmail.com>
2019-06-17 19:59:48 -0400

    generalize adc syncronator script for more than two ads, untested

--- src/adc_synchronator_freq/adc_synchronator_freq.py
+++ src/adc_synchronator_freq/adc_synchronator_freq.py
@@ -10,14 +10,15 @@ from ..pocket_correlator.angle_diff_axis import AngleDiffAxis
 
 class AdcSynchronatorFreq(Experiment):
     """
-    This class is used to synchronize two ADC using
+    This class is used to synchronize two or more ADC using
     frequency domain information.
     """
     def __init__(self, calanfpga):
         Experiment.__init__(self, calanfpga)
         self.bw = self.settings.bw
-        self.nchannels = get_nchannels(self.settings.specsync_info)
+        self.nchannels = get_nchannels(self.settings.spec_info)
         self.freqs = np.linspace(0, self.bw, self.nchannels, endpoint=False)
+        self.sync_regs = self.settings.sync_regs
         
         # test channels array
         chnl_start = self.settings.sync_chnl_start
@@ -33,21 +34,22 @@ class AdcSynchronatorFreq(Experiment):
         self.lo_combination = get_first_lo_combination(self.settings.lo_sources)
 
         # figures and axes
-        self.figure = CalanFigure(n_plots=4, create_gui=False)
-        self.figure.create_axis(0, SpectrumAxis,  self.freqs, 'ZDOK0 spec')
-        self.figure.create_axis(1, SpectrumAxis,  self.freqs, 'ZDOK1 spec')
-        self.figure.create_axis(2, MagRatioAxis,  self.test_freqs, ['z1/z0'], 'Magnitude Ratio')
-        self.figure.create_axis(3, AngleDiffAxis, self.test_freqs, ['z1-z0'], 'Angle Difference')
-        self.figure.set_window_title('ADC Sync')
-        
+        self.n_inputs = len(self.settings.spec_titles)
+        self.figure = CalanFigure(n_plots=self.n_inputs+2, create_gui=False)
+        for i, spec_title in enumerate(self.settings.spec_titles):
+            self.figure.create_axis(i, SpectrumAxis,  self.freqs, spec_title)
+        self.legends = self.settings.corr_legends
+        self.figure.create_axis(self.n_inputs, MagRatioAxis, self.freqs, self.legends, 'Magnitude Ratio')
+        self.figure.create_axis(self.n_inputs+1, AngleDiffAxis, self.freqs, self.legends, 'Angle Difference')
+
     def synchronize_adcs(self):
         """
-        Compute the delay difference between ADC (expected to be 
-        an integer of the smaple time), and apply that delay to the
-        ADC running ahead in order to sychronize both ADCs. This is 
-        frequency based synchronization methods, that is an alternative
-        to the stand-alone time-based adc_synchornator method, with the
-        benefit that is less cumbersome when using backends that require
+        Compute the delay difference between ADCs (expected to be 
+        an integer of the smaple time), and apply that delays to the
+        ADCs running ahead (all except the slowest one) in order to sychronize
+        all ADCs. This is frequency based synchronization methods, that is an 
+        alternative to the stand-alone time-based adc_synchornator method, with 
+        the benefit that is less cumbersome when using backends that require
         down-conversion, and that is more precise.
         """
         init_sources(self.sources)
@@ -59,7 +61,7 @@ class AdcSynchronatorFreq(Experiment):
 
         print "Synchronizing ADCs..."
         while True:
-            spec_ratios = []
+            ratios = [[] for i in range(len(self.legends))]
 
             for i, chnl in enumerate(self.test_channels):
                 # set generator frequency
@@ -68,31 +70,51 @@ class AdcSynchronatorFreq(Experiment):
                 plt.pause(self.settings.pause_time)
 
                 # get power-crosspower data
-                a2, b2 = self.fpga.get_bram_data(self.settings.specsync_info)
-                ab_re, ab_im = self.fpga.get_bram_data(self.settings.crosspowsync_info)
+                pow_data = self.fpga.get_bram_data(self.settings.spec_info)
+                crosspow_data = self.fpga.get_bram_data(self.settings.crosspow_info)
 
-                # compute constant
-                ab = ab_re[chnl] + 1j*ab_im[chnl]
-                spec_ratios.append(np.conj(ab) / a2[chnl]) # (ab*)* / aa* = a*b / aa* = b/a
+                # combine real and imaginary part of crosspow data
+                crosspow_data = np.array(crosspow_data[0::2]) + 1j*np.array(crosspow_data[1::2])
 
-                # plot spec data
-                [a2_plot, b2_plot] = \
-                    self.scale_dbfs_spec_data([a2, b2], self.settings.specsync_info)
-                self.figure.axes[0].plot(a2_plot)
-                self.figure.axes[1].plot(b2_plot)
+                # compute the complex ratios (magnitude ratio and phase difference)
+                # use first input as reference
+                aa = pow_data[0][chnl]
+                for j, ab in enumerate(crosspow_data):
+                    ratios[j].append(np.conj(ab[chnl]) / aa) # (ab*)* / aa* = a*b / aa* = b/a
 
-                # plot magnitude ratio and angle difference
-                self.figure.axes[2].plotxy(self.test_freqs[:i+1], [np.abs(spec_ratios)])
-                self.figure.axes[3].plotxy(self.test_freqs[:i+1], [np.angle(spec_ratios, deg=True)])
+                # plot spectrum
+                spec_data_dbfs = self.scale_dbfs_spec_data(pow_data, self.settings.spec_info)
+                for j, spec in enumerate(spec_data_dbfs):
+                    self.figure.axes[j].plot(spec)
+            
+                # plot the magnitude ratio and phase difference
+                self.figure.axes[-2].plotxy(self.test_freqs[:i+1], np.abs(ratios))
+                self.figure.axes[-1].plotxy(self.test_freqs[:i+1], np.angle(ratios, deg=True))
 
             # plot last frequency
             plt.pause(self.settings.pause_time) 
 
-            delay = self.compute_adc_delay_freq(self.test_freqs[:i+1], spec_ratios) 
+            # get delays between adcs
+            delays = self.compute_adc_delays_freq(self.test_freqs[:i+1], ratios) 
+            
+            # if the sync regs are 1 more the the computed delays,
+            # assume that the first reg is for the reference
+            if len(self.sync_regs)-1 == len(delays):
+                delays.insert(0, 0) # 0 delay for the reference
+
             # check adc sync status, apply delay if needed
-            if delay == 0:
+            if all(delay == 0 for delay in delays):
                 print "ADCs successfully synthronized!"
                 break
+            else:
+                # transform actual delays to necessary delays needed to sync
+                sync_delays = -1*np.array(delays)
+                # offset delays to only get non-negative integers (negative delay do not exist)
+                sync_delays = sync_delays - np.min(sync_delays)
+                # apply delays
+                for sync_delay, sync_regs in zip(sync_delays, self.sync_regs):
+                    self.fpga.set_reg(sync_reg, sync_delay)
+
             elif delay > 0: # if delay is positive adc1 is ahead, hence delay adc1
                 self.fpga.set_reg('adc1_delay', delay)
             else: # (delay < 0) if delay is negative adc0 is ahead, hence delay adc0
@@ -100,23 +122,27 @@ class AdcSynchronatorFreq(Experiment):
 
         turn_off_sources(self.sources)
 
-    def compute_adc_delay_freq(self, freqs, spec_ratios):
+    def compute_adc_delays_freq(self, freqs, ratios):
         """
-        Compute the adc delay between two unsynchronized adcs using information
-        in the frequency domain. It's done by computing the slope of the phase
-        difference with respect the frequency, and then it translates this value
-        into an integer delay in number of samples.
-        :freqs: frequency array in which the sideband ratios where computed.
-        :spec_ratios: spectrometer ratios array of the adcs. The spectrometer
-            ratio is the complex division of an spectral channel from adc0 with adc1.
-        :return: adc delay in number of samples.
+        Compute the adc delay between two or more unsynchronized adcs using 
+        information in the frequency domain. It's done by computing the slope in 
+        the frequency domain of the phase difference between the input of an ADC
+        and a reference signal, and then it translates this value into an 
+        integer delay in number of samples.
+        :freqs: frequency array in which the phase difference slope is computed.
+        :ratios: the complex ratio of the adc inputs and the reference. The 
+            reference is usually one of the ADC inputs.
+        :return: list of the adc delays in number of samples.
         """
-        phase_diffs = np.unwrap(np.angle(spec_ratios))
-        linregress_results = scipy.stats.linregress(freqs, phase_diffs)
-        angle_slope = linregress_results.slope
-        delay = int(round(angle_slope * 2*self.settings.bw / (2*np.pi))) # delay = dphi/df * Fs / 2pi
-        print "Computed delay: " + str(delay)
-        return delay
+        delays = []
+        for adc_ratios in ratios:
+            phase_diffs = np.unwrap(np.angle(adc_ratios))
+            linregress_results = scipy.stats.linregress(freqs, phase_diffs)
+            angle_slope = linregress_results.slope
+            delay = int(round(angle_slope * 2*self.settings.bw / (2*np.pi))) # delay = dphi/df * Fs / 2pi
+            delays.append(delay)
+        print "Computed delays: " + str(delays)
+        return delays
 
 def get_first_lo_combination(lo_sources):
     """
